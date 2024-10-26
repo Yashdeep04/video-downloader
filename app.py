89b4689328b1c945102614856aa14bea53b183b1
Yashdeep <techyone.001@gmail.com>
2024-10-27 02:06:42 +0530

    Add CORS support to backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS  # Import CORS
 import requests
 from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire application
 
 @app.route('/download', methods=['POST'])
 def download_video():
